9cfdd7164688f90c9ece8dd8e1130875ec8d55cf
Tuna Hsu <tunahsu45@gmail.com>
2018-09-21 15:09:54 +0800

    Add Flask-Moment

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
-bootstrap = Bootstrap(app)
 login.login_view = 'login' #'login'為endpoint名，與url_for('login')中的參數是一樣的
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes, models, errors
