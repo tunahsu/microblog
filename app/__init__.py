de86928e5eac6c05344e100b79061886974a198d
Tuna Hsu <tunahsu45@gmail.com>
2018-09-21 15:02:29 +0800

    Use Bootstrap to facelift the front-end

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+bootstrap = Bootstrap(app)
 login.login_view = 'login' #'login'為endpoint名，與url_for('login')中的參數是一樣的
 
 from app import routes, models, errors
